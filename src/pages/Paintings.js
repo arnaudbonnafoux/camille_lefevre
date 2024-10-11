4388af376895af01bd3128bf2750d89b60ca534b
arnaudbonnafoux <bonnafouxarnaud@gmail.com>
2024-10-11 20:47:42 +0200

    modifs_navabar_footer_photos

--- src/pages/Paintings.js
+++ src/pages/Paintings.js
@@ -5,9 +5,9 @@ function Paintings() {
     
     <div>
       <div className="art-item">
-      <img src="images/peinture_1.jpg" alt="Painting 1" class="art-image" />
+      <img src="images/peinture_1.jpg" alt="Painting n°1" class="art-image" />
       <div className="art-description">
-      <h3>Echo des silences</h3>
+      <h3>Éphémère Céleste</h3>
       <p>Une peinture abstraite où les tons de bleu et de vert se mélangent pour évoquer les murmures de la nature. 
       À travers des traits fluides et des textures en relief, Camille Lefèvre invite le spectateur à un voyage introspectif, 
       où chaque détail semble résonner comme un écho lointain.</p>
@@ -15,9 +15,9 @@ function Paintings() {
       </div>
 
       <div className="art-item">
-      <img src="images/peinture_2.jpg" alt="Painting 2" class="art-image" />
+      <img src="images/peinture_2.jpg" alt="Painting n°2" class="art-image" />
       <div className="art-description">
-      <h3>Echo des silences</h3>
+      <h3>Reflet d'Azur</h3>
       <p>Dans cette œuvre, Camille utilise des teintes douces de pastel pour représenter des fragments de mémoire. 
           Les formes floues et les couleurs tamisées créent une atmosphère mélancolique, 
           comme un rêve où les souvenirs se dissolvent peu à peu dans le temps.</p>
@@ -25,9 +25,9 @@ function Paintings() {
       </div>
 
       <div className="art-item">
-      <img src="images/peinture_3.jpg" alt="Painting 3" class="art-image" />
+      <img src="images/peinture_3.jpg" alt="Painting n°3" class="art-image" />
       <div className="art-description">
-      <h3>Echo des silences</h3>
+      <h3>Murmure de l'Aube</h3>
       <p>Cette peinture joue sur les contrastes entre ombre et lumière, avec des touches de jaune vif qui tranchent sur un fond sombre. 
           Camille cherche à capter l'éphémère, 
           ces instants fugaces où la lumière révèle une nouvelle dimension du monde qui nous entoure.</p>
--- src/pages/Sculptures.js
+++ src/pages/Sculptures.js
@@ -4,9 +4,9 @@ function Sculptures() {
   return (
     <div>
       <div class="art-item">
-      <img src="images/sculpture_1.jpg" alt="Sculpture 1" class="art-image" />
+      <img src="images/sculpture_1.jpg" alt="Sculpture n°1" class="art-image" />
         <div class="art-description">
-        <h3>Echo des silences</h3>
+        <h3>Équilibre Éternel</h3>
         <p>Cette sculpture en terre cuite représente un arbre majestueux dont les racines s'enroulent autour d'une pierre. 
           Elle évoque la connexion profonde entre la nature et l'humanité. 
           Les détails minutieux des feuilles et des branches rappellent la beauté fragile de l'environnement naturel. 
@@ -15,9 +15,9 @@ function Sculptures() {
       </div>
 
       <div class="art-item">
-      <img src="images/sculpture_2.jpg" alt="Sculpture 2" class="art-image" />
+      <img src="images/sculpture_2.jpg" alt="Sculpture n°2" class="art-image" />
       <div class="art-description">
-      <h3>Echo des silences</h3>
+      <h3>Souffle de l'Âme</h3>
       <p>Faite en acier inoxydable, cette sculpture moderne se dresse comme un hommage aux paysages urbains. 
           Ses lignes épurées et géométriques évoquent la structure des bâtiments et des routes. 
           La réflexion de la lumière sur sa surface métallique crée un jeu d'ombres et de reflets, symbolisant la dynamique de la vie en ville. 
@@ -26,9 +26,9 @@ function Sculptures() {
       </div>
 
       <div class="art-item">
-      <img src="images/sculpture_3.jpg" alt="Sculpture 3" class="art-image" />
+      <img src="images/sculpture_3.jpg" alt="Sculpture n°3" class="art-image" />
       <div class="art-description">
-      <h3>Echo des silences</h3>
+      <h3>Ondes Silencieuses</h3>
       <p>Cette sculpture en bois sculpté représente un mouvement fluide et gracieux, comme si la matière était animée par une force invisible. 
           Les courbes et les formes organiques de l'œuvre inspirent un sentiment de légèreté et de sérénité. 
           Cette pièce évoque l'idée que la beauté peut résider dans l'éphémère et l'intangible, 
