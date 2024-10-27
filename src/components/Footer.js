8a8b0f93faacdf93e1e280e66f3ccb8461ca6063
arnaudbonnafoux <bonnafouxarnaud@gmail.com>
2024-10-27 17:44:51 +0100

    modif_footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import './Footer.css'; // Importation du fichier CSS pour le footer
+import './Footer.css';
+import { ReactComponent as Logo } from './instagram_icon.svg';
+import { ReactComponent as Logo_2 } from './mail_icon.svg';
 
 function Footer() {
   return (
     <footer className="footer">
-      <p>Contact : </p>
-      <p>
-        <a href="mailto:contact@camillelefevre.com" className="footer-email">contact@camillelefevre.com</a>
+      <p className="footer-contact">
+        <a href="mailto:contact@camillelefevre.com" className="footer-email"><Logo_2 
+        className="footer-logo" style={{ fill: '#05034F'}}/></a>
+        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" 
+        className="footer-icon">
+          <Logo className="footer-logo" style={{ fill: '#05034F'}}/>
+        </a>
       </p>
-      <p> &copy; 2024 duanrA. Ce projet est sous <a href="https://opensource.org/licenses/MIT" target="_blank" className='footer-email'>licence MIT</a>.
-        </p>
+      <p>&copy; 2024 duanrA. Ce projet est sous <a href="https://opensource.org/licenses/MIT" 
+      target="_blank" className="footer-email">licence MIT</a>.</p>
     </footer>
   );
 }
