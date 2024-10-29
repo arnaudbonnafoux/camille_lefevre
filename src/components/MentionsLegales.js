db0ca804a0e1c3697f48c9cba7bea355f4bb8fe5
arnaudbonnafoux <bonnafouxarnaud@gmail.com>
2024-10-29 18:23:23 +0100

    modif_navbar

--- src/components/MentionsLegales.js
+++ src/components/MentionsLegales.js
@@ -5,7 +5,7 @@ import './NormesFooter.css';
 
 const MentionsLegales = () => (
   <div className='text-height'>
-    <h1>Mentions Légales</h1>
+    <h3>Mentions Légales</h3>
     <p>Éditeur : duanrA</p>
     <p>Contact : duanrA@gmail.com</p>
     <p>Hébergement : GitHub Pages, GitHub, Inc.</p>
--- src/components/PolitiqueConfidentialite.js
+++ src/components/PolitiqueConfidentialite.js
@@ -5,7 +5,7 @@ import './NormesFooter.css';
 
 const PolitiqueConfidentialite = () => (
   <div className='text-height'>
-    <h1>Politique de Confidentialité</h1>
+    <h3>Politique de Confidentialité</h3>
     <p>Ce site ne collecte aucune donnée personnelle, ni cookies.</p>
     <p>Pour toute question, veuillez contacter : duanrA@gmail.com</p>
   </div>
